a6944ecda91174b7b10dadb34e7b79e8cc9049cf
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-01-09 21:16:19 +0200

    Function to get a random character from the provided set

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const lower = "abcdefghijklmnopqrstuvwxyz";
 const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+[]{}|;:,.<>?";
+
+function getRandomCharacter(set) {
+  return set[Math.floor(Math.random() * set.length)];
+}
